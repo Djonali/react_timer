f98f06d164ea544518b42514b57d0cd894b09edd
djonali <flexjonali@gmail.com>
2024-07-30 16:10:18 +0530

    Updated label

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,7 +2,6 @@ import { useState, useRef } from 'react';
 
 import ResultModal from './ResultModal.jsx';
 
-// let timer;
 
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
